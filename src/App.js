114732066f1656b647a69695e9e83fbf857d6229
Maher Wardak <maher.wardak@gmail.com>
2021-04-01 21:34:48 -0400

    WIP handleSumbit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const App = () => {
   //remove todo's
   //create state to hold list of todo's
   const [todos, setTodos] = useState([]);
+  const [expenseItem, setExpenseItem] = useState('');
 
   //add todo's to list
   const handleOnChange = (event) => {
@@ -14,7 +15,10 @@ const App = () => {
   // map through list of todo's and reuturn list items (li). this should be inside of a ul/ol
   const handleSubmit = (event) => {
     event.preventDefault();
-   
+    setExpenseItem(event.target.value)
+    setTodos({...todos, ...expenseItem});
+    debugger;
+    
   };
 
   const storedList = todos.map((todo) => {
