8d48a64880082ce0e4ac934c16b2f856c347bff5
Maher Wardak <maher.wardak@gmail.com>
2021-04-02 15:52:18 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -3,31 +3,22 @@ import "./App.css";
 
 const App = () => {
   //remove todo's
-  //create state to hold list of todo's
+  //create 2 pieces of state: 1 . to store newtodo & 2. to store the list of todos
+  const [newTodo, setNewTodo] = useState("");
   const [todos, setTodos] = useState([]);
-  const [expenseItem, setExpenseItem] = useState('');
 
   //add todo's to list
   const handleOnChange = (event) => {
-    setTodos(event.target.value);
+    const input = event.target.value;
+    setNewTodo(input);
   };
 
   // map through list of todo's and reuturn list items (li). this should be inside of a ul/ol
   const handleSubmit = (event) => {
     event.preventDefault();
-    setExpenseItem(event.target.value)
-    setTodos({...todos, ...expenseItem});
-    debugger;
-    
+    const todoItems = [...todos, newTodo];
+    setTodos(todoItems);
   };
-
-  const storedList = todos.map((todo) => {
-    return (
-      <ul>
-        <li>{todo}</li>
-      </ul>
-    )
-  });
 
   return (
     <div className="container">
@@ -44,14 +35,16 @@ const App = () => {
                 value={todos}
                 required
               />
-              <button id="add-btn" >
-                Add
-              </button>
+              <button id="add-btn">Add</button>
             </div>
           </form>
         </div>
         <div className="item-list">
-          <ul id="list">{storedList}</ul>
+          <ol id="list">
+            {todos.map((todo) => {
+              return <li>{todo}</li>;
+            })}
+          </ol>
         </div>
         <button id="clear-btn" style={{ visibility: "visible" }}>
           Clear List
