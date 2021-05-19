4feafa1e7eb69f3ab4a8608c7ecd61dc80a427f1
Maher Wardak <maher.wardak@gmail.com>
2021-05-18 22:14:06 -0400

    localStorage completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 
-
 //
 
 const App = () => {
@@ -10,18 +9,30 @@ const App = () => {
   const [inputValue, setinputValue] = useState("");
   const [todos, setTodos] = useState([]);
 
+
   
 
-  //handle the change of state
-  const handleOnChange = (event) => {
-    const input = event.target.value;
-    setinputValue(input);
-  };
+  //get todos from local storage
+  useEffect(() => {
+    const todos =
+      JSON.parse(localStorage.getItem("todos")) || [];
+    setTodos(todos);
+  }, []);
 
+  //save todos to local storage
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
+    //handle the change of state
+    const handleOnChange = (event) => {
+      const input = event.target.value;
+      setinputValue(input);
+    };
+  
   // handle submission of todos that are entered and store in a new array and update prev state
   const handleSubmit = (event) => {
     event.preventDefault();
-
 
     //Use spread operator to reference todos and setTodos new todo to end of array
     const newTodos = [
@@ -29,12 +40,7 @@ const App = () => {
       { text: inputValue, done: false, id: Math.random().toFixed(3) * 512 },
     ];
     setTodos(newTodos);
-    
-    //
-
     setinputValue("");
-
-    
   };
   // Clear entire todos array
   const clearArr = (e) => {
@@ -46,33 +52,11 @@ const App = () => {
     e.preventDefault();
     //filter through each item.done in the array
     setTodos(todos.filter((todo) => todo.id != id));
-    // to delete an item check if todo.id (which are todos in state) are not equal to the item being deleted
-    // this will return all a new array of all the items that are not equal to id and state is updated with the new array
-    // this is will have 1 less item from the old array because once the logic is false and todo.id does equal id that item
-    // will not be returned in the new array
-    //filter through each item.done in the array
-    //if checkbox is checked then item.done === true,  set style to strike through
-    //then when button is clicked it should remove item from array
   };
- 
-//retrieve todos from local storage
-  const getData = () => { 
-    const localData = localStorage.getItem("todos");
-   return localData ? JSON.parse(localData) : [];
-  };
-
-  //save todos to local storage
-useEffect(() => {
-  localStorage.setItem("todos", JSON.stringify(todos))
-  getData();
-},  [todos]);
-
 
   return (
     <form onSubmit={handleSubmit}>
-      
       <div className="container">
-     
         <div className="card">
           <div className="header">
             <h1>NWR Tasks</h1>
@@ -101,7 +85,6 @@ useEffect(() => {
                       className="delete"
                       onClick={(e) => deleteItem(e, todo.id)}
                     >
-                    
                       <i className="fas fa-trash"></i>
                     </button>
                   </div>
